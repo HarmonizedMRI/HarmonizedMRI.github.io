8d4252fe614c1ade24ff674f907173297c7d7dc3
RyanZurrin <ryanzurrin@ryanzurrin.com>
2024-08-13 15:16:39 -0400

    fixed issue with words being cut off in project descriptions.

--- assets/js/expand-collapse.js
+++ assets/js/expand-collapse.js
@@ -1,12 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const projectList = document.getElementById('projectList');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const projectList = document.getElementById('projectList');
+//
+//   projectList.addEventListener('click', function(e) {
+//     if (e.target && e.target.classList.contains('read-more')) {
+//       e.preventDefault();
+//       const description = e.target.previousElementSibling;
+//       description.classList.toggle('expanded');
+//       e.target.textContent = description.classList.contains('expanded') ? 'Less' : 'Read more';
+//     }
+//   });
+// });
+document.addEventListener("DOMContentLoaded", function() {
+  const descriptions = document.querySelectorAll('.project-item .description');
 
-  projectList.addEventListener('click', function(e) {
-    if (e.target && e.target.classList.contains('read-more')) {
-      e.preventDefault();
-      const description = e.target.previousElementSibling;
-      description.classList.toggle('expanded');
-      e.target.textContent = description.classList.contains('expanded') ? 'Less' : 'Read more';
+  descriptions.forEach(description => {
+    const readMore = description.nextElementSibling; // The "Read more" link
+
+    if (description.clientHeight < description.scrollHeight) {
+      readMore.style.display = 'inline'; // Show the "Read more" link
+
+      readMore.addEventListener('click', function(e) {
+        e.preventDefault();
+        if (description.classList.contains('expanded')) {
+          description.classList.remove('expanded');
+          readMore.textContent = 'Read more';
+        } else {
+          description.classList.add('expanded');
+          readMore.textContent = 'Read less';
+        }
+      });
     }
   });
 });
